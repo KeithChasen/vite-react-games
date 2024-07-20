4d1d10bed93e61989efe8ab3317dd934dfd297ad
keithchasen <keithchasen89@gmail.com>
2024-07-20 21:22:03 +0200

    refactored: Player: movement logic moved to separate methods

--- src/gameEngines/Fighting/Player.js
+++ src/gameEngines/Fighting/Player.js
@@ -9,7 +9,7 @@ export class Player extends Fighter {
         this.controls = new Controls();
     }
 
-    update() {
+    downAction() {
         if (this.controls?.down && this.position.y) {
             this.height = 200; // temporarily simulate fighter is down (sitting)
 
@@ -33,7 +33,9 @@ export class Player extends Fighter {
                 this.position.y = this.canvas?.height - 500;
             }
         }
+    }
 
+    upAction() {
         if (
             this.controls?.up && 
             this.position.y !== null && 
@@ -43,44 +45,35 @@ export class Player extends Fighter {
                 this.verticalAcceleration === 0 && 
                 this.position.y >= (this.canvas.height - 500)
             ) {
-                this.controls?.stopUp();
                 this.verticalAcceleration = 50;
             }
         }
+    }
 
-        if (this.position.x !== null) {
-            if (this.controls?.left) {
-                if (this.position.x > 0) {
-                    this.position.x -= 10;
-                } else if (this.position.x <= 0) {
-                    this.position.x = 0;
-                }
-            }
-
-            if (this.controls?.right && this.canvas) {
-                if (this.position.x + this.width < this.canvas?.width) {
-                    this.position.x += 10;
-                } else if (this.position.x + this.width >= this.canvas?.width) {
-                    this.position.x = this.canvas?.width - this.width;
-                }
-            }
-        }
-
-
-        // if (this.controls?.up && this.position.y !== null) {
-        //     this.position.y -= 10;
-        // }
-
-        // if (this.controls?.down && this.position.y !== null) {
-        //     this.position.y += 10;
-        // }
-
+    leftAction() {
         if (this.controls?.left && this.position.x !== null) {
-            this.position.x -= 10;
+            if (this.position.x > 0) {
+                this.position.x -= 10;
+            } else if (this.position.x <= 0) {
+                this.position.x = 0;
+            }
         }
+    }
 
-        if (this.controls?.right && this.position.x !== null) {
-            this.position.x += 10;
+    rightAction() {
+        if (this.controls?.right && this.canvas && this.position.x !== null) {
+            if (this.position.x + this.width < this.canvas?.width) {
+                this.position.x += 10;
+            } else if (this.position.x + this.width >= this.canvas?.width) {
+                this.position.x = this.canvas?.width - this.width;
+            }
         }
+    }
+
+    update() {
+        this.downAction();
+        this.upAction();
+        this.leftAction();
+        this.rightAction(); 
     }
 }
