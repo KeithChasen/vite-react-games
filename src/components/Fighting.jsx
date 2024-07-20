8114d3e5ea808bf52b14d84a28ed941ea249cebb
keithchasen <keithchasen89@gmail.com>
2024-07-20 20:07:56 +0200

    set up canvas

--- src/components/Fighting.jsx
+++ src/components/Fighting.jsx
@@ -1,7 +1,23 @@
+import { useRef } from "react";
+
 function Fighting() {
+    const canvasRef = useRef(null);
+    const canvas = canvasRef?.current;
+    const c = canvas?.getContext('2d');
+
+    if (canvas) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        console.log({
+            canvas, c
+        });
+    }
+
     return (
         <>
             Fighting
+            <canvas ref={canvasRef}></canvas>
         </>
     );
 }
