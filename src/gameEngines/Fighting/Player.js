34d907916720a90a48fa8b4f312d576355f5bfcf
keithchasen <keithchasen89@gmail.com>
2024-07-20 21:17:39 +0200

    simulate sit functionality

--- src/gameEngines/Fighting/Player.js
+++ src/gameEngines/Fighting/Player.js
@@ -10,6 +10,30 @@ export class Player extends Fighter {
     }
 
     update() {
+        if (this.controls?.down && this.position.y) {
+            this.height = 200; // temporarily simulate fighter is down (sitting)
+
+            if (
+                this.canvas &&
+                !this.verticalAcceleration &&
+                this.position.y >= this.canvas?.height - 500 - this.height
+            ) {
+                this.position.y = this.canvas?.height - 500 + 200;
+            }
+        }
+
+        if (!this.controls?.down && this.position.y) {
+            this.height = 400; // temporarily simulate fighter is up
+
+            if (
+                this.canvas &&
+                !this.verticalAcceleration &&
+                this.position.y >= this.canvas?.height - 500
+            ) {
+                this.position.y = this.canvas?.height - 500;
+            }
+        }
+
         if (
             this.controls?.up && 
             this.position.y !== null && 
@@ -23,14 +47,33 @@ export class Player extends Fighter {
                 this.verticalAcceleration = 50;
             }
         }
-        
-        if (this.controls?.up && this.position.y !== null) {
-            this.position.y -= 10;
+
+        if (this.position.x !== null) {
+            if (this.controls?.left) {
+                if (this.position.x > 0) {
+                    this.position.x -= 10;
+                } else if (this.position.x <= 0) {
+                    this.position.x = 0;
+                }
+            }
+
+            if (this.controls?.right && this.canvas) {
+                if (this.position.x + this.width < this.canvas?.width) {
+                    this.position.x += 10;
+                } else if (this.position.x + this.width >= this.canvas?.width) {
+                    this.position.x = this.canvas?.width - this.width;
+                }
+            }
         }
 
-        if (this.controls?.down && this.position.y !== null) {
-            this.position.y += 10;
-        }
+
+        // if (this.controls?.up && this.position.y !== null) {
+        //     this.position.y -= 10;
+        // }
+
+        // if (this.controls?.down && this.position.y !== null) {
+        //     this.position.y += 10;
+        // }
 
         if (this.controls?.left && this.position.x !== null) {
             this.position.x -= 10;
